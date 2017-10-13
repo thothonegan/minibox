b3a3042099dfa8fdfa83078e646f438570078743
Kenneth Perry (thothonegan) <thothonegan@gmail.com>
2017-10-13 16:11:11 -0500

    Added some source code

---
+++ src/minibox.core/Application.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minibox.core
+{
+    class Application
+    {
+        static void PrintHello (string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.ReadKey();
+        }
+    }
+}
